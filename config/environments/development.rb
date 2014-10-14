075ca8ac442279c26c52771d886889e62b9c88d8
Tran Van Thanh <tranthanhepu@gmail.com>
2014-10-14 08:17:33 +0700

    config email server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,17 +38,6 @@ Rails.application.configure do
   # Config email server
   # Coding by: tranthanhepu
   # Date 2014/10/13
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'mail.grooovyware.jp',
-  #   port:                 25,
-  #   domain:               'tranthanhepu.com',
-  #   user_name:            'egc',
-  #   password:             'egcpass',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
-
-
   config.action_mailer.raise_delivery_errors = true # still no logs about emails
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true # I can't believe I have to add this option. Does it even exist? I found it on google.
